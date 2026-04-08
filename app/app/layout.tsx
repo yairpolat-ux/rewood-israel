1ba76eac0283c36e72ab3e8e1712fd9428d61610
yairpolat-ux <yairpolat@gmail.com>
2026-04-08 22:51:05 +0300

    add layout

---
+++ app/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "ReWood Israel",
+  description: "Created by Yair",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <body>{children}</body>
+    </html>
+  )
+}
